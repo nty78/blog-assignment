2d3f46a81272d3f997cc2a117373c4a6f3334e6f
Nuisang <tyodpayut@gmail.com>
2026-06-22 16:47:58 +0700

    fix: update next config to use export default for mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // ปิดการตรวจสอบ ESLint ตอนรัน Build เพื่อให้ผ่านไปได้ก่อน
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // ปิดการตรวจสอบ TypeScript ตอน Build ในกรณีที่ Types มันงงๆ
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = nextConfig;
+// เปลี่ยนจาก module.exports เป็น export default
+export default nextConfig;
