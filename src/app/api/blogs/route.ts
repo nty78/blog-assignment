e573c64074b4f8e08cd3e566669f6dfe1a01cc71
Nuisang <tyodpayut@gmail.com>
2026-06-22 15:08:57 +0700

    fix: add force-dynamic to blogs route

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic'; // 
+
 // ------------------------------------------------------
 // Method GET: สำหรับดึงข้อมูล Blog ทั้งหมดไปแสดงที่หน้าบ้าน
 // ------------------------------------------------------
